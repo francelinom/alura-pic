64d0ac7bd3d1f6724b357b1442bd9b38502a0746
francelino <fmarcilio1989@gmail.com>
2022-03-03 21:54:40 -0300

    Adicionando o routerLink para login

--- src/app/core/core.module.ts.module.ts
+++ src/app/core/core.module.ts.module.ts
@@ -1,9 +1,10 @@
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   declarations: [HeaderComponent],
   exports: [HeaderComponent],
 })
