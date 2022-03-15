eae284c7e19dd0433a771d1f47a361fb6ce93181
francelino <fmarcilio1989@gmail.com>
2022-03-14 22:05:38 -0300

    Adicionando o component de footer

--- src/app/core/core.module.ts.module.ts
+++ src/app/core/core.module.ts.module.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule],
-  declarations: [HeaderComponent],
-  exports: [HeaderComponent],
+  declarations: [HeaderComponent, FooterComponent],
+  exports: [HeaderComponent, FooterComponent],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
---
+++ src/app/core/footer/footer.component.ts
@@ -0,0 +1,18 @@
+import { UserService } from './../user/user.service';
+import { User } from './../user/user';
+import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css'],
+})
+export class FooterComponent implements OnInit {
+  user$!: Observable<User>;
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.user$ = this.userService.getUser();
+  }
+}
