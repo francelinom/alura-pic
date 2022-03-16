0d0b7438d6b2d1592578a0dd78ba751cb487df7d
francelino <fmarcilio1989@gmail.com>
2022-03-15 21:33:27 -0300

    Adicionando o template de adicionar foto

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -1,9 +1,11 @@
+import { VmessageModule } from './../../shared/vmessage/vmessage.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PhotoFormComponent } from './photo-form.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [PhotoFormComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, VmessageModule, FormsModule],
 })
 export class PhotoFormModule {}
