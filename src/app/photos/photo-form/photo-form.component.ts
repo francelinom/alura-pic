2c776afc7a9522d678e220a22e28fe125b3eb11a
francelino <fmarcilio1989@gmail.com>
2022-03-16 21:58:21 -0300

    Adicionando o serviço para carregar foto

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -1,3 +1,5 @@
+import { Router } from '@angular/router';
+import { PhotoService } from './../photo/photo.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
@@ -10,7 +12,11 @@ export class PhotoFormComponent implements OnInit {
   photoForm!: FormGroup;
   file!: File | any;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private photoService: PhotoService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.photoForm = this.formBuilder.group({
@@ -23,7 +29,11 @@ export class PhotoFormComponent implements OnInit {
   upload() {
     const description = this.photoForm.get('description')?.value;
     const allowComments = this.photoForm.get('allowComments')?.value;
-    const dados = this.photoForm.getRawValue();
+    this.photoService
+      .upload(description, allowComments, this.file)
+      .subscribe(() => {
+        this.router.navigate(['']);
+      });
   }
 
   onChange(target: any) {
